9b26dfe574221d3d7e329d474fe18c3fe70c45e2
NikolasMelui <slak@samaradom.ru>
2018-04-12 02:54:36 +0400

    Add new functional which read a queryString parameters from request and stringify it.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import url from 'url';
 const server = http.createServer((req, res) => {
 	const parsedUrl = url.parse(req.url, true);
 	const path = parsedUrl.pathname;
+	const queryStringObject = parsedUrl.query;
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 	const method = req.method.toLocaleUpperCase();
 	res.end(`Path is: ${trimmedPath}\n`);
-	global.console.log(`Request received on path: ${trimmedPath} with ${method} method.`);
+	global.console.log(
+		`Request received on path: ${trimmedPath} with ${method} method and these query parameters: ${JSON.stringify(
+			queryStringObject
+		)}.`
+	);
 });
 
 server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
