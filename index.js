244ddd6673dee817241331ac8bf42fbb96150b70
NikolasMelui <slak@samaradom.ru>
2018-04-12 01:58:36 +0400

    Clear and add new boilerplate.

--- index.js
+++
@@ -1,6 +0,0 @@
-import http from 'http';
-import config from './config';
-
-const server = http.createServer((req, res) => res.end('Hello there!'));
-
-server.listen(3000, () => global.console.log(`Server is listening on ${config.server.port}`));
