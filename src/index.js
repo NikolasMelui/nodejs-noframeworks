c3c848b12084c6a74f2719adbdeea3b6b3a9d12a
NikolasMelui <slak@samaradom.ru>
2018-04-12 02:44:45 +0400

    Add new feature to define a http method.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const server = http.createServer((req, res) => {
 	const parsedUrl = url.parse(req.url, true);
 	const path = parsedUrl.pathname;
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+	const method = req.method.toLocaleUpperCase();
 	res.end(`Path is: ${trimmedPath}\n`);
-	global.console.log(`Request received on path: ${trimmedPath}`);
+	global.console.log(`Request received on path: ${trimmedPath} with ${method} method.`);
 });
 
 server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
