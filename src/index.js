e3edde7c345f7afdfc8da03bd27e260747fbbe6c
NikolasMelui <slak@samaradom.ru>
2018-04-12 01:59:54 +0400

    Change helloworld text.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => res.end('Welcome to multikey-node-boilerplate!'));
+const server = http.createServer((req, res) => res.end('Welcome to new project!'));
 
 server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
