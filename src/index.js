d09203952ae16ada8bc4210a3aa6b731c9ec7b8f
NikolasMelui <slak@samaradom.ru>
2018-04-12 02:37:37 +0400

    Add new code for url module - parseUrl and pathname of requesting url path.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import http from 'http';
+import url from 'url';
 
-const server = http.createServer((req, res) => res.end('Welcome to new project!'));
+const server = http.createServer((req, res) => {
+	const parsedUrl = url.parse(req.url, true);
+	const path = parsedUrl.pathname;
+	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+	res.end(`Path is: ${trimmedPath}\n`);
+	global.console.log(`Request received on path: ${trimmedPath}`);
+});
 
 server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
