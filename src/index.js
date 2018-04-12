0462e434e59a3bbcfe355a2839b10ac7b3a8b3d8
NikolasMelui <slak@samaradom.ru>
2018-04-13 00:01:54 +0400

    Add new headers code on server and client.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ const server = http.createServer((req, res) => {
 	const path = parsedUrl.pathname;
 	const queryStringObject = parsedUrl.query;
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-	const method = req.method.toLocaleUpperCase();
-	res.end(`Path is: ${trimmedPath}\n`);
+	const method = req.method.toUpperCase();
+	const headersObject = req.headers;
+	let headersString = '';
+	for (const [key, value] of Object.entries(headersObject)) {
+		headersString += `${key} => ${value}\n`;
+	}
 	global.console.log(
 		`Request received on path: ${trimmedPath} with ${method} method and these query parameters: ${JSON.stringify(
 			queryStringObject
-		)}.`
+		)}`
 	);
+	global.console.log(`And headers is\n`, headersObject);
+	res.end(`Path is: ${trimmedPath}\nHeaders is:\n${headersString}`);
 });
 
 server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
