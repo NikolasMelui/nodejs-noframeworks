01cfb062d1d774f4cba1d9afc7c2bfc45ffee34c
NikolasMelui <slak@samaradom.ru>
2018-04-13 00:51:09 +0400

    Add parsing payloads method. Add new stream 'on' listeners. Add new StringDecoder object.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import url from 'url';
+import { StringDecoder } from 'string_decoder';
 
 const server = http.createServer((req, res) => {
 	const parsedUrl = url.parse(req.url, true);
@@ -12,13 +13,21 @@ const server = http.createServer((req, res) => {
 	for (const [key, value] of Object.entries(headersObject)) {
 		headersString += `${key} => ${value}\n`;
 	}
-	global.console.log(
-		`Request received on path: ${trimmedPath} with ${method} method and these query parameters: ${JSON.stringify(
-			queryStringObject
-		)}`
-	);
-	global.console.log(`And headers is\n`, headersObject);
-	res.end(`Path is: ${trimmedPath}\nHeaders is:\n${headersString}`);
+
+	const decoder = new StringDecoder('utf-8');
+	let buffer = '';
+
+	req.on('data', data => (buffer += decoder.write(data)));
+	req.on('end', () => {
+		buffer += decoder.end();
+		global.console.log(
+			`Request received on path: ${trimmedPath}\nwith method: ${method}\nquery parameters: ${JSON.stringify(
+				queryStringObject
+			)}\npayloads: ${buffer}`
+		);
+		global.console.log(`and headers:\n`, headersObject);
+		res.end(`Path: ${trimmedPath}\nHeaders:\n${headersString}`);
+	});
 });
 
 server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
