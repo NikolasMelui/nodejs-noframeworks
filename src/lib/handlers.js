99eac34e34061905ebb6cd80dc40b91c4f4666ee
NikolasMelui <slak@samaradom.ru>
2018-05-03 10:21:30 +0400

    Add new handlers signature. Add new subhandlers signature. Add 4 new sub_users subhandlers.

--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -1,14 +1,38 @@
 const handlers = {
+	/**
+	 * Handlers
+	 *
+	 */
 	users: (data, callback) => {
 		const acceptableMethods = ['post', 'get', 'put', 'delete'];
 		if (acceptableMethods.indexOf(data.method) > -1) {
-			handlers._users[data.method](data, callback);
+			handlers.sub_users[data.method](data, callback);
 		} else {
 			callback(405);
 		}
 	},
 	ping: (data, callback) => callback(200, { res: 'server is working' }),
 	notFound: (data, callback) => callback(404),
+
+	/**
+	 * Subhandlers
+	 * There is NO underscore good practice on airbnb eslint rules, so use sub_*
+	 *
+	 */
+	sub_users: {
+		post: (data, callback) => {
+			callback();
+		},
+		get: (data, callback) => {
+			callback();
+		},
+		put: (data, callback) => {
+			callback();
+		},
+		delete: (data, callback) => {
+			callback();
+		},
+	},
 };
 
 export default handlers;
