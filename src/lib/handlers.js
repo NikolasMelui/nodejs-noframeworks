299b42e62c1915d41d8c1e950eae1e7f28adadf9
NikolasMelui <slak@samaradom.ru>
2018-05-08 10:18:06 +0400

    Add sub_users post method body.

--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -21,7 +21,30 @@ const handlers = {
 	 */
 	sub_users: {
 		post: (data, callback) => {
-			callback();
+			const firstName =
+				typeof data.payload.firstName === 'string' && data.payload.firstName.trim().length > 0
+					? data.payload.firstName.trim()
+					: false;
+			const lastName =
+				typeof data.payload.lastName === 'string' && data.payload.lastName.trim().length > 0
+					? data.payload.lastName.trim()
+					: false;
+			const phone =
+				typeof data.payload.phone === 'string' && data.payload.phone.trim().length === 10
+					? data.payload.phone.trim()
+					: false;
+			const password =
+				typeof data.payload.password === 'string' && data.payload.password.trim().length > 0
+					? data.payload.password.trim()
+					: false;
+			const tosAgreement =
+				typeof data.payload.tosAgreement === 'boolean' && data.payload.tosAgreement === true
+					? data.payload.tosAgreement
+					: false;
+			if (firstName && lastName && phone && password && tosAgreement) {
+			} else {
+				callback(400, { Error: 'Missing required fields.' });
+			}
 		},
 		get: (data, callback) => {
 			callback();
