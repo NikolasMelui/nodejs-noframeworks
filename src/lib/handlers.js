85e3e6fbbabd67a8a8b4604cecfbd0037e33a1d0
NikolasMelui <slak@samaradom.ru>
2018-05-10 12:16:34 +0400

    Add new userObject assignment. Add new _data.create function call in handlers.js file.

--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -24,30 +24,46 @@ const handlers = {
 	 */
 	sub_users: {
 		post: (data, callback) => {
-			const firstName =
+			const curFirstName =
 				typeof data.payload.firstName === 'string' && data.payload.firstName.trim().length > 0
 					? data.payload.firstName.trim()
 					: false;
-			const lastName =
+			const curLastName =
 				typeof data.payload.lastName === 'string' && data.payload.lastName.trim().length > 0
 					? data.payload.lastName.trim()
 					: false;
-			const phone =
+			const curPhone =
 				typeof data.payload.phone === 'string' && data.payload.phone.trim().length === 10
 					? data.payload.phone.trim()
 					: false;
-			const password =
+			const curPassword =
 				typeof data.payload.password === 'string' && data.payload.password.trim().length > 0
 					? data.payload.password.trim()
 					: false;
-			const tosAgreement =
+			const curTosAgreement =
 				typeof data.payload.tosAgreement === 'boolean' && data.payload.tosAgreement === true
 					? data.payload.tosAgreement
 					: false;
-			if (firstName && lastName && phone && password && tosAgreement) {
-				_data.read('users', phone, (err, data) => {
+			if (curFirstName && curLastName && curPhone && curPassword && curTosAgreement) {
+				_data.read('users', curPhone, (err, data) => {
 					if (err) {
-						const hashPassword = helpers.hash(password); // @TODO: Create new helpers file and hash function in it.
+						const curHashPassword = helpers.hash(curPassword);
+						// Create new user object
+						const userObject = {
+							firstName: curFirstName,
+							lastName: curLastName,
+							phone: curPhone,
+							hashedPassword: curHashPassword,
+							tosAgreement: true,
+						};
+						_data.create('users', curPhone, userObject, _err => {
+							if (!_err) {
+								callback(200);
+							} else {
+								global.console.log(_err);
+								callback(500, { Error: 'Could not create the new user.' });
+							}
+						});
 					} else {
 						callback(400, { Error: 'The user with that phone number is already exist.' });
 					}
