ca7453ef671dd9d22f07f3190f5559c11af2c20b
NikolasMelui <slak@samaradom.ru>
2018-06-06 10:55:32 +0400

    Add payload signature is helpers.sendTwilioSms method.

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -33,6 +33,21 @@ const helpers = {
 		}
 		return false;
 	},
+	sendTwilioSms: (phone, message, callback) => {
+		phone = typeof phone === 'string' && phone.trim().length === 10 ? phone.trim() : false;
+		message =
+			typeof msg === 'string' && phone.trim().length > 0 && phone.trim().length <= 1600 ? phone.trim() : false;
+		if (phone && message) {
+			// Configure the request payload
+			const payload = {
+				From: config.twilio.fromPhone,
+				To: `+1 ${config.twilio.toPhone}`,
+				Body: message,
+			};
+		} else {
+			callback('Given parameters were missing or invalid');
+		}
+	},
 };
 
 export default helpers;
