5fec92c117c37482bc303ff59671af5363c76172
NikolasMelui <slak@samaradom.ru>
2018-06-10 15:26:32 +0400

    Add workers.gatherAllChecks method signature. Add workers.loop method.

--- src/lib/workers.js
+++ src/lib/workers.js
@@ -12,12 +12,21 @@ import https from 'https';
 import helpers from './helpers';
 import _data from './data';
 
-// Init the worker
+// Init the workers
 const workers = {
+	// Lookup all checks, get their data, send to a validator
+	gatherAllChecks: () => {},
+
+	// Timer to execute the the worker-process once per minute
+	loop: () => {
+		setInterval(workers.gatherAllChecks(), 1000 * 60);
+	},
+
 	// Init script
 	init: () => {
 		// Execute all the checks immediately
 		workers.gatherAllChecks();
+
 		// Call the loop so the checks will execute later on
 		workers.loop();
 	},
