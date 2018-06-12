541848ed76c9f425edd72c101c2d8ae08c310f18
NikolasMelui <slak@samaradom.ru>
2018-06-12 23:49:50 +0400

    Add sanity checker for the check-data in workers.validateCheckData method.

--- src/lib/workers.js
+++ src/lib/workers.js
@@ -20,18 +20,62 @@ const workers = {
 		_data.list('checks', (err, checks) => {
 			if (!err && checks && checks.length > 0) {
 				checks.forEach(curCheck => {
+					// Read in the check data
 					_data.read('checks', curCheck, (_err, originalCheckData) => {
 						if (!_err && originalCheckData) {
+							// Pass it to the check validator, and let that function continue or
 							workers.validateCheckData(originalCheckData);
 						} else {
 							global.console.log(`Error reading one of the check's data`);
 						}
 					});
 				});
+			} else {
+				global.console.log('Error: Could not find any checks or processes');
 			}
 		});
 	},
 
+	// Sanity check the check-data
+	validateCheckData: originalCheckData => {
+		const curOriginalCheckData =
+			typeof originalCheckData === 'object' && originalCheckData !== null ? originalCheckData : {};
+		curOriginalCheckData.id =
+			typeof originalCheckData.id === 'string' && originalCheckData.id.trim().length === 20
+				? originalCheckData.id
+				: false;
+		curOriginalCheckData.userPhone =
+			typeof originalCheckData.userPhone === 'string' && originalCheckData.userPhone.trim().length === 10
+				? originalCheckData.userPhone
+				: false;
+		curOriginalCheckData.protocol =
+			typeof originalCheckData.protocol === 'string' && ['http', 'https'].indexOf(originalCheckData.protocol) > -1
+				? originalCheckData.protocol
+				: false;
+		curOriginalCheckData.url =
+			typeof originalCheckData.url === 'string' && originalCheckData.url.trim().length > 0
+				? originalCheckData.url
+				: false;
+		curOriginalCheckData.method =
+			typeof originalCheckData.method === 'string' &&
+			['post', 'get', 'put', 'delete'].indexOf(originalCheckData.method) > -1
+				? originalCheckData.method
+				: false;
+		curOriginalCheckData.successCodes =
+			typeof originalCheckData.successCodes === 'object' &&
+			originalCheckData.successCodes instanceof Array &&
+			originalCheckData.successCodes.length > 0
+				? originalCheckData.successCodes
+				: false;
+		curOriginalCheckData.timeoutSeconds =
+			typeof originalCheckData.timeoutSeconds === 'number' &&
+			originalCheckData.timeoutSeconds % 1 === 0 &&
+			originalCheckData.timeoutSeconds >= 1 &&
+			originalCheckData.timeoutSeconds <= 5 &&
+			originalCheckData.timeoutSeconds.length > 0
+				? originalCheckData.timeoutSeconds
+				: false;
+	},
 	// Timer to execute the the worker-process once per minute
 	loop: () => {
 		setInterval(workers.gatherAllChecks(), 1000 * 60);
