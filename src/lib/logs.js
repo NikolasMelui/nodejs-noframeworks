abf06634734171c769d75090c4ec764e64b9915f
NikolasMelui <slak@samaradom.ru>
2018-07-21 14:31:51 +0400

    Add compress method to the logs.js file.

--- src/lib/logs.js
+++ src/lib/logs.js
@@ -3,7 +3,7 @@
  */
 import fs from 'fs';
 import path from 'path';
-// import zlib from 'zlib';
+import zlib from 'zlib';
 
 const logs = {
 	baseDir: path.join(__dirname, '../data/logs'),
@@ -32,6 +32,7 @@ const logs = {
 			}
 		});
 	},
+
 	// List all the logs and optionally include the compressed logs
 	list: (includeCompressedLogs, callback) => {
 		fs.readdir(logs.baseDir, (err, data) => {
@@ -50,6 +51,49 @@ const logs = {
 				callback(false, trimmedFileNames);
 			} else {
 				callback(err, data);
+			}
+		});
+	},
+
+	// Compress the contents of the .log file into the .gz.b64 file within the same directory
+	compress: (logId, newFileId, callback) => {
+		const sourceFile = `${logId}.log`;
+		const destFile = `${newFileId}.gz.b64`;
+
+		// Read the source file
+		fs.readFile(`${logs.baseDir}${sourceFile}`, 'utf8', (err, inputString) => {
+			if (!err && inputString) {
+				// Compress the data using gzip
+				zlib.gzip(inputString, (__err, buffer) => {
+					if (!__err && buffer) {
+						// Send the data to the destination file
+						fs.open(`${logs.baseDir}${destFile}wx`, (___err, fileDesctiptor) => {
+							if (!___err && fileDesctiptor) {
+								// Write to the destination file
+								fs.writeFile(fileDesctiptor, buffer.toString('base64'), ____err => {
+									if (!____err) {
+										// Close the destination file
+										fs.close(fileDesctiptor, _____err => {
+											if (!_____err) {
+												callback(false);
+											} else {
+												callback(_____err);
+											}
+										});
+									} else {
+										callback(____err);
+									}
+								});
+							} else {
+								callback(___err);
+							}
+						});
+					} else {
+						callback(__err);
+					}
+				});
+			} else {
+				callback(err);
 			}
 		});
 	},
