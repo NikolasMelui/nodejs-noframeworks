0f4094ebb77b2090acb20dbb9bb43ba186c86e36
nikolasmelui <slak@samaradom.ru>
2018-12-05 10:59:12 +0400

    Add new XMLHttpRequest and form it like an AJAX in jQuery

--- src/public/app.js
+++ src/public/app.js
@@ -2,13 +2,13 @@
  * Frontend logic for the application
  */
 
-const app = {
+const apps = {
   config: {
     sessionToken: false
   },
   // AJAX client for the restful API
   client: {
-    requust: (headers, path, method, queryStringObject, payload, callback) => {
+    request: (headers, path, method, queryStringObject, payload, callback) => {
       // Set defaults
       headers = typeof headers === 'object' && headers !== null ? headers : {};
       path = typeof path === 'string' ? path : '/';
@@ -38,6 +38,41 @@ const app = {
       }
       // Join requestUrl array to the string
       requestUrl = requestUrl.join('');
+
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, requestUrl, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      // For each header sent, add it to the request
+      for (let headerKey in headers) {
+        if (headers.hasOwnProperty(headerKey)) {
+          xhr.setRequestHeader(headerKey, headers[headerKey]);
+        }
+      }
+
+      // If there is a current session token set, add that as a header
+      if (app.config.sessionToken) {
+        xhr.setRequestHeader('token', app.config.sessionToken.id);
+      }
+
+      // When the request come back, handle the response
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+          const statusCode = xhr.status;
+          const responseReturned = xhr.responseText;
+
+          // Callback if requested
+
+          if (callback) {
+            try {
+              const parsedResponse = JSON.parse(responseReturned);
+              callback(statusCode, parsedResponse);
+            } catch (error) {
+              callback(statusCode, false);
+            }
+          }
+        }
+      };
     }
   }
 };
