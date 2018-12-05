c2636afeee37a6ee4939a9f593ae00f678668c6f
nikolasmelui <slak@samaradom.ru>
2018-12-05 10:18:35 +0400

    Get  signatures for the methods on the client side

--- src/public/app.js
+++ src/public/app.js
@@ -2,6 +2,14 @@
  * Frontend logic for the application
  */
 
-const app = {};
+const app = {
+  config: {
+    sessionToken: false
+  },
+  // AJAX client for the restful API
+  client: {
+    requust: (headers, path, method, queryStringObject, payload, callback) => {}
+  }
+};
 
 console.log('Hello there!');
