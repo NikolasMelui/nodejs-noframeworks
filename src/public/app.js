aba004769014c61e3cc7e5e6234f9919e829ea0d
nikolasmelui <slak@samaradom.ru>
2018-12-05 10:28:45 +0400

    Set defaults to the client request

--- src/public/app.js
+++ src/public/app.js
@@ -8,7 +8,22 @@ const app = {
   },
   // AJAX client for the restful API
   client: {
-    requust: (headers, path, method, queryStringObject, payload, callback) => {}
+    requust: (headers, path, method, queryStringObject, payload, callback) => {
+      // Set defaults
+      headers = typeof headers === 'object' && headers !== null ? headers : {};
+      path = typeof path === 'string' ? path : '/';
+      method =
+        typeof method === 'string' &&
+        ['POST', 'GET', 'PUT', 'DELETE'].includes(method)
+          ? method.toUpperCase()
+          : 'GET';
+      queryStringObject =
+        typeof queryStringObject === 'object' && queryStringObject !== null
+          ? queryStringObject
+          : {};
+      payload = typeof payload === 'object' && payload !== null ? payload : {};
+      callback = typeof callback === 'function' ? callback : false;
+    }
   }
 };
 
