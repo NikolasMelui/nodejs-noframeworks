5fb3021134851640bc914254c08c09a687bbf84a
nikolasmelui <slak@samaradom.ru>
2018-12-18 02:04:35 +0400

    Add new sessionCreate handler in the handlers.js file

--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -74,6 +74,34 @@ const handlers = {
     }
   },
 
+  // Create Session
+  sessionCreate: (data, callback) => {
+    if (data.method === 'get') {
+      // Prepare data for interpolation
+      const templateData = {
+        'haad.title': 'Login to your Account',
+        'head.discription':
+          'Please enter your phone number and password to access your account.',
+        'body.class': 'sessionCreate'
+      };
+      // Read in a template as a string
+      helpers.getTemplate('sessionCreate', templateData, (err, str) => {
+        if (!err && str) {
+          // Add the universal header and footer
+          helpers.addUniversalTemplates(str, templateData, (_err, _str) => {
+            if (!_err && _str) {
+              callback(200, _str, 'html');
+            } else {
+              callback(500, undefined, 'html');
+            }
+          });
+        } else {
+          callback(500, undefined, 'html');
+        }
+      });
+    }
+  },
+
   // Favicon handler
   favicon: (data, callback) => {
     if (data.method === 'get') {
